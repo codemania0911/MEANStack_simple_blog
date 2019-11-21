475d9437ff04b3e5020df3f842e94f3d4cd7b916
slarkjm0803 <54478769+slarkjm0803@users.noreply.github.com>
2019-11-21 17:53:36 +0800

    Update auth.guard.ts

--- app/auth.guard.ts
+++ app/auth.guard.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { RegistrationService } from 'src/app/registration.service';
 
-
 @Injectable()
 export class AuthGuard implements CanActivate {
     
