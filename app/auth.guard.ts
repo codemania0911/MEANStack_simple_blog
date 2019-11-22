b022541e57cfa47ed02a347284251674a3e85330
slarkjm0803 <54478769+slarkjm0803@users.noreply.github.com>
2019-11-22 09:10:15 +0800

    Update auth.guard.ts

--- app/auth.guard.ts
+++ app/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
             return true;
         }else {
             this._router.navigate(['index'])
-            return false
+            return false;
         }
     }
 }
