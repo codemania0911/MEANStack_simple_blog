efc0eb6648d420fc3542a0e7512a7d6e0816f500
slarkjm0803 <54478769+slarkjm0803@users.noreply.github.com>
2020-03-05 00:30:01 +0800

    Update index.component.ts

--- app/index/index.component.ts
+++ app/index/index.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
